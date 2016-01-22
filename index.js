686a906ec9ee23420e13b162a7e9bc9fef0d3df3
youngwind <540506594@qq.com>
2016-01-22 15:38:04 +0800

    通过添加fs模块，根据url读取返回静态资源

--- index.js
+++ index.js
@@ -4,20 +4,39 @@
  */
 
 var http = require("http");
+var fs = require("fs");
+
+function serveStaticFile(res, path, contentType, responseCode) {
+  if (!responseCode) {
+    responseCode = 200;
+  }
+  console.log(__dirname);
+  fs.readFile(__dirname + path, function (err, data) {
+    if (err) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end('500 - Internal Error');
+    } else {
+      res.writeHead(responseCode, {'Content-Type': contentType});
+      res.end(data);
+    }
+  });
+}
+
 http.createServer(function (req, res) {
   var path = req.url.toLowerCase();
   console.log(path);
-  switch (path){
+  switch (path) {
     case '/':
-      res.writeHead(200, {"Content-Type":"text/plain"});
-      res.end("HomePage");
+      serveStaticFile(res, '/public/home.html', 'text/html');
       break;
     case '/about':
-      res.writeHead(200, {"Content-Type":"text/plain"});
-      res.end("About");
+      serveStaticFile(res, '/public/about.html', 'text/html');
+      break;
+    case "/img/logo.png":
+      serveStaticFile(res, '/public/logo.png', 'image/png');
       break;
     default :
-      res.writeHead(400, {"Content-Type":"text/plain"});
+      res.writeHead(400, {"Content-Type": "text/plain"});
       res.end("Not Found");
       break;
   }

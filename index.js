4660586e2fdcc896321ed2074fb18058e5dd2d1b
youngwind <540506594@qq.com>
2016-01-22 15:07:11 +0800

    根据不同的url返回不同的文本

--- index.js
+++ index.js
@@ -5,8 +5,22 @@
 
 var http = require("http");
 http.createServer(function (req, res) {
-  res.writeHead(200, {"Content-Type": "text/plain"});
-  res.end("Hello World!");
+  var path = req.url.toLowerCase();
+  console.log(path);
+  switch (path){
+    case '/':
+      res.writeHead(200, {"Content-Type":"text/plain"});
+      res.end("HomePage");
+      break;
+    case '/about':
+      res.writeHead(200, {"Content-Type":"text/plain"});
+      res.end("About");
+      break;
+    default :
+      res.writeHead(400, {"Content-Type":"text/plain"});
+      res.end("Not Found");
+      break;
+  }
 }).listen(process.env.PORT || 5000);
 
 

9fd80d8000a495a35ef06b126356d7b50b86e3f0
youngwind <540506594@qq.com>
2016-01-22 20:13:36 +0800

    1、添加express 2、使用express代替node 的http模块返回文本

--- index.js
+++ index.js
@@ -3,45 +3,31 @@
  * Hello World
  */
 
-var http = require("http");
-var fs = require("fs");
+var express = require('express');
+var app = express();
 
-function serveStaticFile(res, path, contentType, responseCode) {
-  if (!responseCode) {
-    responseCode = 200;
-  }
-  console.log(__dirname);
-  fs.readFile(__dirname + path, function (err, data) {
-    if (err) {
-      res.writeHead(500, {'Content-Type': 'text/plain'});
-      res.end('500 - Internal Error');
-    } else {
-      res.writeHead(responseCode, {'Content-Type': contentType});
-      res.end(data);
-    }
-  });
-}
+app.set('port', process.env.PORT || 5000);
 
-http.createServer(function (req, res) {
-  var path = req.url.toLowerCase();
-  console.log(path);
-  switch (path) {
-    case '/':
-      serveStaticFile(res, '/public/home.html', 'text/html');
-      break;
-    case '/about':
-      serveStaticFile(res, '/public/about.html', 'text/html');
-      break;
-    case "/img/logo.png":
-      serveStaticFile(res, '/public/logo.png', 'image/png');
-      break;
-    default :
-      res.writeHead(400, {"Content-Type": "text/plain"});
-      res.end("Not Found");
-      break;
-  }
-}).listen(process.env.PORT || 5000);
+// 定制404页面
+app.get('/', function (req, res) {
+  res.type('text/plain');
+  res.send('Meadowalrt Travel');
+});
 
+app.get('/about', function (req, res) {
+  res.type('text/plain');
+  res.send('About');
+});
+
+app.use(function (req, res, next) {
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 not-found');
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Express started on http://localhost:' + app.get('port') + ';Press Ctrl-C to terminate.');
+});
 
 
 

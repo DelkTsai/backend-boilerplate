57f0e9de0901b797a0c7295a6b4adf5a9d9492b1
youngwind <540506594@qq.com>
2016-01-22 10:34:24 +0800

    添加动态端口号

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var http = require("http");
 http.createServer(function (req, res) {
   res.writeHead(200, {"Content-Type": "text/plain"});
   res.end("Hello World!");
-}).listen(3333);
+}).listen(process.env.PORT || 5000);
 
 
 

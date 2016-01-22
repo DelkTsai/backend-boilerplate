3d43c0808dbf245ed7bee13b4858f98b92bcef4c
youngwind <540506594@qq.com>
2016-01-22 09:00:04 +0800

    helloworld

---
+++ hello-world.js
@@ -0,0 +1,12 @@
+/**
+ * Created by youngwind on 16/1/22.
+ * Hello World
+ */
+
+var http = require("http");
+http.createServer(function (req, res) {
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end("Hello World!");
+}).listen(3333);
+
+
